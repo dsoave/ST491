4485e8b8f272df3515cc832108063fd4ea6bae64
dsoave <18703690+dsoave@users.noreply.github.com>
2024-12-16 11:10:36 -0500

    Update RegressionReviewWithR.R

--- in-class-demos/RegressionReviewWithR.R
+++ in-class-demos/RegressionReviewWithR.R
@@ -54,3 +54,69 @@ anova(justsize, fullmodel)  # Full vs reduced
 
 # Compare country effects controlling for size
 anova(justcountry, fullmodel)
+
+……………. Remove!!!!!
+# General linear test approach. H0: L beta = h
+
+source("http://www.utstat.utoronto.ca/brunner/Rfunctions/ftest.txt")
+
+# Test country controlling for size: Compare F = 6.8999
+# Full model again for comparison
+summary(fullmodel)
+
+# Now the F-test of couuntry controlling for size
+L0 = rbind(c(0,0,0,1,0),
+            c(0,0,0,0,1))
+L0
+
+ftest(fullmodel,L0)
+
+# As before, t-tests give comparison of U.S with Europe and Japan.
+# Test Europe vs. Japan controlling for size.
+L1 = cbind(0,0,0,1,-1) # One row, 5 columns
+ftest(fullmodel,L1)
+
+###### Predictions, confidence intervals and prediction intervals ######
+
+# Predict litres per 100 km for a Japanese car weighing
+# 1295kg, 4.52m long (1990 Toyota Camry)
+
+b = fullmodel$coefficients; b
+
+ell = c(1,1295,4.52,1,0)
+yhat = sum(ell*b); # ell-prime b
+yhat
+
+# Confidence interval for E(ell-prime beta)
+# First the hard way
+
+tcrit = qt(0.975,df=fullmodel$df.residual) # t_alpha/2
+MSE.XpXinv = vcov(fullmodel)
+ell = as.matrix(ell) # Now it's a column vector
+me95 = tcrit * sqrt( as.numeric(t(ell) %*% MSE.XpXinv %*% ell) )
+lower95 = yhat - me95; upper95 = yhat + me95
+c(lower95, upper95) # 95% Confidence interval for ell-prime beta
+
+# Use the predict function
+# help(predict.lm)
+
+camry1990 = data.frame(weight=1295,length=4.52,Cntry='Japan')
+camry1990
+predict(fullmodel,newdata=camry1990) # Compare yhat = 12.38739
+predict(fullmodel,newdata=camry1990, interval='confidence')
+
+# With 95 percent prediction interval (95 is default)
+
+predict(fullmodel,newdata=camry1990, interval='prediction')
+
+# Multiple predictions
+cadillac1990 = data.frame(weight=1800,length=5.22,Cntry='US')
+volvo1990 = data.frame(weight=1371,length=4.823,Cntry='Europ')
+newcars = rbind(camry1990,cadillac1990,volvo1990); newcars
+
+is.data.frame(newcars)
+predict(fullmodel,newdata=newcars, interval='prediction')
+
+
+
+
