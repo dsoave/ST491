e06aacc17e04e933e284fbbc1c24bffd8431b3f8
dsoave <18703690+dsoave@users.noreply.github.com>
2025-02-13 11:05:57 -0500

    Update Part8_RegressionReviewWithR.R

--- in-class-demos/Part8_RegressionReviewWithR.R
+++ in-class-demos/Part8_RegressionReviewWithR.R
@@ -1,80 +1,81 @@
-# Title: Review: Normal Regression with R*
-
-# Step 1: Load the Data
-# Load the dataset directly from GitHub
-kars <- read.table("https://raw.githubusercontent.com/dsoave/Datasets/refs/heads/main/mcars4.data")
-dim(kars)  # Check dimensions
-head(kars) # View first few rows
-summary(kars) # Summary statistics
-
-# Step 2: Create Dummy Variables
-# Create dummy variables for the 'Cntry' column
-n <- dim(kars)[1]
-kars <- within(kars, {
-  c1 <- numeric(n); c1[Cntry == 'Europ'] <- 1
-  c2 <- numeric(n); c2[Cntry == 'Japan'] <- 1
-  c3 <- numeric(n); c3[Cntry == 'US'] <- 1
-  Cntry <- factor(Cntry)
-})
-
-# Validate the results
+kars = read.table("https://raw.githubusercontent.com/dsoave/Datasets/refs/heads/main/mcars4.data")
+dim(kars)
 head(kars)
 summary(kars)
 
-# Step 3: Validate Dummy Variables
-# Check the dummy variables
-with(kars, table(c1, Cntry))
-with(kars, table(c2, Cntry))
-with(kars, table(c3, Cntry))
+n = dim(kars)[1]; n
+# Modifying the data frame, not the original data file
+kars = within(kars,{
++ # Make dummy variables
++ c1 = numeric(n); c1[Cntry=='Europ'] = 1
++ c2 = numeric(n); c2[Cntry=='Japan'] = 1
++ c3 = numeric(n); c3[Cntry=='US'] = 1
++ # Make Cntry a factor
++ Cntry = factor(Cntry)
++ }) # End of within kars
 
-# Step 4: Analyze Mean Fuel Consumption
-# Mean fuel consumption by country
-with(kars, aggregate(lper100k, by = list(Cntry), FUN = mean))
+head(kars)
+summary(kars)
 
-# Step 5: Fit a Linear Regression Model
-# Test if fuel consumption differs by country
-justcountry <- lm(lper100k ~ c1 + c2, data = kars)
+# Checking dummy variables
+with(kars,table(c1,Cntry))
+with(kars,table(c2,Cntry))
+with(kars,table(c3,Cntry))
+
+# Take a look at mean fuel consumption for each country
+with(kars, aggregate(lper100k,by=list(Cntry),FUN=mean) )
+
+# Must specify a LIST of grouping factors
+
+
+# H0: mu1=mu2=mu3
+justcountry = lm(lper100k ~ c1+c2, data=kars)
 summary(justcountry)
 
-# Step 6: Change Reference Categories
-# Change reference category to "US"
-kars$Country <- kars$Cntry
-contrasts(kars$Country) <- contr.treatment(3, base = 3)
-summary(lm(lper100k ~ Country, data = kars))
+# Which means are different?
+# Have t-tests. What about Europe vs. Japan?
+# test H0: beta1 = beta2
+# A cheap way is to use a different reference category.
+# R can make the dummy variables for you
+is.factor(kars$Cntry)
+# The factor Cntry has dummy vars built in. What are they?
+contrasts(kars$Cntry) # Note alphabetical order
 
-# Step 7: Add Covariates to the Model
-# Full model with covariates
-fullmodel <- lm(lper100k ~ weight + length + Country, data = kars)
-summary(fullmodel)
+jc2 = lm(lper100k~Cntry, data=kars); summary(jc2)
 
-# Step 8: Compare Models
-# Reduced model for comparison
-justsize <- lm(lper100k ~ weight + length, data = kars)
-anova(justsize, fullmodel)  # Full vs reduced
 
-# Compare country effects controlling for size
-anova(justcountry, fullmodel)
+# You can select the dummy variable coding scheme and the reference category.
+contr.treatment(3,base=2) # Category 2 is the reference category
 
-……………. Remove!!!!!
-# General linear test approach. H0: L beta = h
 
-source("http://www.utstat.utoronto.ca/brunner/Rfunctions/ftest.txt")
+# U.S. as reference category again
+kars$Country = kars$Cntry
+contrasts(kars$Country) = contr.treatment(3,base=3)
+summary( lm(lper100k~Country, data=kars) )
 
-# Test country controlling for size: Compare F = 6.8999
-# Full model again for comparison
-summary(fullmodel)
 
-# Now the F-test of couuntry controlling for size
-L0 = rbind(c(0,0,0,1,0),
-            c(0,0,0,0,1))
-L0
+# Names of dummy variables 1=Europe, 2=Japan could be nicer
+colnames(contrasts(kars$Country)) = c("Europe","Japan")
+contrasts(kars$Country)
 
-ftest(fullmodel,L0)
+summary( lm(lper100k~Country, data=kars) )
+# Include covariates
+fullmodel = lm(lper100k ~ weight+length+Country, data=kars)
+summary(fullmodel) # Look carefully at the signs!
 
-# As before, t-tests give comparison of U.S with Europe and Japan.
-# Test Europe vs. Japan controlling for size.
-L1 = cbind(0,0,0,1,-1) # One row, 5 columns
-ftest(fullmodel,L1)
+
+# Test country controlling for size, using full versus reduced approach.
+
+justsize = lm(lper100k ~ weight+length, data=kars); summary(justsize)
+
+with(kars, cor(weight,length)  )
+
+
+# I advise using anova ONLY to compare full and reduced models
+anova(justsize,fullmodel) # Full vs reduced
+
+# Test car size controlling for country too -- why not?
+anova(justcountry,fullmodel)
 
 ###### Predictions, confidence intervals and prediction intervals ######
 
@@ -116,6 +117,9 @@ newcars = rbind(camry1990,cadillac1990,volvo1990); newcars
 
 is.data.frame(newcars)
 predict(fullmodel,newdata=newcars, interval='prediction')
+
+
+
 
 
 
