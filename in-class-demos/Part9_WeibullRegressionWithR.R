90dfa94d153d79331d6078e8d4fbf5752f745a2f
dsoave <18703690+dsoave@users.noreply.github.com>
2025-02-28 11:44:44 -0500

    Create Part9_WeibullRegressionWithR.R

---
+++ in-class-demos/Part9_WeibullRegressionWithR.R
@@ -0,0 +1,8 @@
+# Part9_WeibullRegressionWithR.R
+
+rm(list=ls()); # options(scipen=999)
+# install.packages("survival",dependencies=TRUE) # Only need to do this once
+library(survival) # Do this every time
+# install.packages("asaur",dependencies=TRUE) # Only need to do this once
+library(asaur)
+# help(pharmacoSmoking)
