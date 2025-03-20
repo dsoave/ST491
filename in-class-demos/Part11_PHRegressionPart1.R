86198f93b884c366e0bf17ee74abe05a35f7b7e5
dsoave <18703690+dsoave@users.noreply.github.com>
2025-03-20 09:29:17 -0400

    Update Part11_PHRegressionPart1.R

--- in-class-demos/Part11_PHRegressionPart1.R
+++ in-class-demos/Part11_PHRegressionPart1.R
@@ -7,20 +7,35 @@ beta0 = 2.3; beta1 = 0.05
 # Simulate one data set
 set.seed(9999); options(scipen=999)
 x = rnorm(n,mean=mux,sd=sdx)
+hist(x)
 epsilon = rexp(n)
+hist(epsilon)
 tstar = exp(beta0 + beta1*x) * epsilon
+hist(tstar)
 U = abs(rnorm(n, mean=0, sd=100)) # Censoring times
+hist(U)
 t = pmin(tstar,U) # Vector of minima, n values
+
 delta = as.numeric(tstar<U); table(delta)
 
 # Try exponential regression and proportional hazards regression
 stime = Surv(t,delta)
+plot(survfit(stime~1))
+plot(survfit(stime~(x>50)))
 
 expo = survreg(stime ~ x , dist='exponential')
 summary(expo)
+beta0;beta1
+
 
 phaz = coxph(stime ~ x)
 summary(phaz)
+
+expo = survreg(stime ~ x , dist='weibull')
+summary(expo)
+
+
+
 
 # Pharmaco-smoking
 rm(list=ls());  options(scipen=999)
@@ -38,13 +53,14 @@ quit = within(pharmacoSmoking,{
  Race[Race!='white'] = 'blackOther'; Race=factor(Race)
  }) # Finished making data frame quit
 
-w_All = survreg(DayOfRelapse ~ grp + age + gender + Race + employment + yearsSmoking + levelSmoking + priorAttempts, dist='weibull', data=quit); summary(w_All)
+head(quit)
+w_All = survreg(DayOfRelapse ~ grp + age +  employment + yearsSmoking + levelSmoking + priorAttempts, dist='weibull', data=quit); summary(w_All)
 
-ph_All = coxph(DayOfRelapse ~ grp + age + gender + Race + employment + yearsSmoking + levelSmoking + priorAttempts, data=quit); summary(ph_All)
+ph_All = coxph(DayOfRelapse ~ grp + age +  employment + yearsSmoking + levelSmoking + priorAttempts, data=quit); summary(ph_All)
 
 # Checking the re-parameterization, a Weibull -beta_j/sigma should be close to a proportional hazards beta_j. Look at Racewhite.
 
--0.25145/1.72  # Compare  -0.1394286 from PH
+-1.092580/1.72  # Compare  -0.1394286 from PH
 # Not bad. This is n=125
 
 wfull =  survreg(DayOfRelapse ~ grp + age + employment , dist='weibull', data=quit)
@@ -54,9 +70,7 @@ phfull =  coxph(DayOfRelapse ~ grp + age + employment, data=quit)
 summary(phfull)
 
 # How are they getting the confidence intervals for those hazard ratios?
-L = -0.60788 -1.96*0.21837; L
 
-exp(L)
 
 # Try Partial Likelihood and Wald tests for employment, controlling for age and
 # experimental treatment.
