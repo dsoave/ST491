aa17ac141225cecbbc58da52a5a5c8872ac46399
dsoave <18703690+dsoave@users.noreply.github.com>
2025-03-21 15:29:09 -0400

    Update Part12_PHRegressionPart2.R

--- in-class-demos/Part12_PHRegressionPart2.R
+++ in-class-demos/Part12_PHRegressionPart2.R
@@ -2,6 +2,8 @@ rm(list=ls());  # options(scipen=999)
 # install.packages("survival",dependencies=TRUE) # Only need to do this once
 library(survival) # Do this every time
 
+help(kidney) ### A description of the data set
+
 # help(kidney)
 head(kidney)
 summary(kidney)
@@ -12,7 +14,6 @@ table(kidney$disease)
 
 contrasts(kidney$disease)
 
-with(Kidney,cor(age,frail))
 
 # Make a new data frame with
      # 1=F, 0=M
@@ -26,10 +27,8 @@ Kidney = within(kidney,{
  })
 with(Kidney,cor(age,frail))
 
-kmod1 = coxph( Surv(time,status) ~ age + sex + disease + frail, data=Kidney)
+kmod1 = coxph( Surv(time,status) ~ age + sex + disease, data=Kidney)
 summary(kmod1)
-# Are se(coef) labelled correctly?
-se = sqrt(diag(vcov(kmod1))); se # Yes
 # Are se(coef) labelled correctly?
 se = sqrt(diag(vcov(kmod1))); se # Yes
 
@@ -45,39 +44,29 @@ exp(CIbeta1)
 
 summary(kmod1)
 
-# Estimated hazard of infection is ____ times as great for women as men.
-# Estimated hazard of infection is ____ times as great for disease type AN as it is for Other.
-# Estimated hazard of infection is ____ times as great for disease type AN as it is for disease type PKD.
+# (1) Estimated hazard of infection is ____ times as great for female as male
+# (2) Estimated hazard of infection is ____ times as great for disease type AN as it is for Other.
+# (3) Estimated hazard of infection is ____ times as great for disease type AN as it is for disease type PKD.
 betahat = coef(kmod1); betahat
 
 exp(betahat[4]-betahat[5]) # Hazard ratio of AN/PKD
 
 
 # Test disease type with  a partial likeihood ratio test
-k2 = coxph( Surv(time,status) ~ age + sex + frail, data=Kidney)
+k2 = coxph( Surv(time,status) ~ age + sex, data=Kidney)
 anova(k2,kmod1)
-
-# Wald test: function(L,Tn,Vn,h=0) # H0: L theta = h
-Vn_hat = vcov(kmod1); betahat
-
-LL = rbind(c(0,0,1,0,0,0),
-             c(0,0,0,1,0,0),
-             c(0,0,0,0,1,0)  )
-round( Wtest(LL,betahat,Vn_hat), 5)
 
 
 # Comparing survival functions for males and females
-guy = data.frame(age=0, sex=0, disease="Other", frail=0) # An average guy
-gal = data.frame(age=0, sex=1, disease="Other", frail=0) # An average gal
-sexcomp = rbind(guy,gal); sexcomp
+male = data.frame(age=0, sex=0, disease="Other") # An average guy
+female = data.frame(age=0, sex=1, disease="Other") # An average gal
+sexcomp = rbind(male,female); sexcomp
 
 rownames(sexcomp) = c("M","F"); sexcomp
 
 s1 = survfit(kmod1,newdata=sexcomp)
 s1
 
-s1 = survfit(kmod1,newdata=sexcomp)
-s1
 
 ls(s1)
 
@@ -85,22 +74,25 @@ summary(s1)
 
 head(s1$cumhaz)
 
-ls(s1)
-
 
 S = s1$surv[1:10,1] # Col 1 is males
 H = s1$cumhaz[1:10,1]
 Q = exp(-H) # Question: Is this the survival function?
 cbind(H,S,Q)
 
+
+#Check that Shat(t) for females is S0hat(t)^exp(beta1hat)
+cbind(s1$surv[1:10,1],s1$surv[1:10,2],s1$surv[1:10,1]^exp(-1.483137252 ))
+
+
 plot(s1)
+
+
 # Try to locate the medians
 xx = c(0,400); yy = c(.5,0.5)
 lines(xx,yy,col="red")
 # Median for M = 26, F = 141 ?
 
-
-# Median for M = 26, F = 141 ?
 xm = c(26,26); ym = c(0,1); lines(xm,ym,col="red")
 xf = c(141,141); yf = c(0,1); lines(xf,yf,col="blue")
 
@@ -113,6 +105,9 @@ text(500,0.9,"Males  ")
 
 xf = c(350,450); yf = c(0.8,0.8); lines(xf,yf,lty=2)
 text(500,0.8,"Females")
+
+
+
 # Compare disease types, just for women
 table(Kidney$disease)
 
@@ -133,3 +128,20 @@ s2
 
 plot(s2,lty = 1:4,xlab="Days", ylab="Probability")
 title('Estimated "Survival" Probabilities by Disease Type')
+### Which disease type has the obvious favourable prognosis?
+
+
+### Compare the estimated survival curves using coxph and 
+### the Breslow estimate of H0(t)
+### VS the survival curves estimate from the KM approach
+### Both approaches stratify by sex only
+kmod1 = coxph( Surv(time,status) ~ sex, data=Kidney)
+plot(survfit(kmod1,newdata=sexcomp))
+# KM Curves for the two sex categories.
+lines(survfit(Surv(time,status) ~ sex ,data=Kidney),lty=2)
+
+
+kmod1 = coxph( Surv(time,status) ~ 1, data=Kidney)
+plot(survfit(kmod1),conf.int = FALSE)
+# KM Curves for the two sex categories.
+lines(survfit(Surv(time,status) ~ 1 ,data=Kidney),lty=2,conf.int = F)
