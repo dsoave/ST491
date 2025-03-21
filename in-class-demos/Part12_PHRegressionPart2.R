df83998ebc60482506e3f95a917099e9b828822f
dsoave <18703690+dsoave@users.noreply.github.com>
2025-03-21 14:22:37 -0400

    Update Part12_PHRegressionPart2.R

--- in-class-demos/Part12_PHRegressionPart2.R
+++ in-class-demos/Part12_PHRegressionPart2.R
@@ -1,1 +1,135 @@
-aaa
+rm(list=ls());  # options(scipen=999)
+# install.packages("survival",dependencies=TRUE) # Only need to do this once
+library(survival) # Do this every time
+
+# help(kidney)
+head(kidney)
+summary(kidney)
+
+dim(kidney)
+
+table(kidney$disease)
+
+contrasts(kidney$disease)
+
+with(Kidney,cor(age,frail))
+
+# Make a new data frame with
+     # 1=F, 0=M
+     # age and frailty centered
+
+Kidney = within(kidney,{
+ sex = sex-1 # Indicator for female
+ # Centering age and frailty
+ age = age-mean(age)
+ frail = frail-mean(frail)
+ })
+with(Kidney,cor(age,frail))
+
+kmod1 = coxph( Surv(time,status) ~ age + sex + disease + frail, data=Kidney)
+summary(kmod1)
+# Are se(coef) labelled correctly?
+se = sqrt(diag(vcov(kmod1))); se # Yes
+# Are se(coef) labelled correctly?
+se = sqrt(diag(vcov(kmod1))); se # Yes
+
+# CI for the hazard ratio exp(beta1)
+betahat = coef(kmod1); betahat
+
+CIbeta1 = c(betahat[1]-1.96*se[1], betahat[1]+1.96*se[1]); CIbeta1
+
+exp(CIbeta1)
+# So summary is giving us confidence intervals for the hazard ratios,
+# not the coefficients.
+
+
+summary(kmod1)
+
+# Estimated hazard of infection is ____ times as great for women as men.
+# Estimated hazard of infection is ____ times as great for disease type AN as it is for Other.
+# Estimated hazard of infection is ____ times as great for disease type AN as it is for disease type PKD.
+betahat = coef(kmod1); betahat
+
+exp(betahat[4]-betahat[5]) # Hazard ratio of AN/PKD
+
+
+# Test disease type with  a partial likeihood ratio test
+k2 = coxph( Surv(time,status) ~ age + sex + frail, data=Kidney)
+anova(k2,kmod1)
+
+# Wald test: function(L,Tn,Vn,h=0) # H0: L theta = h
+Vn_hat = vcov(kmod1); betahat
+
+LL = rbind(c(0,0,1,0,0,0),
+             c(0,0,0,1,0,0),
+             c(0,0,0,0,1,0)  )
+round( Wtest(LL,betahat,Vn_hat), 5)
+
+
+# Comparing survival functions for males and females
+guy = data.frame(age=0, sex=0, disease="Other", frail=0) # An average guy
+gal = data.frame(age=0, sex=1, disease="Other", frail=0) # An average gal
+sexcomp = rbind(guy,gal); sexcomp
+
+rownames(sexcomp) = c("M","F"); sexcomp
+
+s1 = survfit(kmod1,newdata=sexcomp)
+s1
+
+s1 = survfit(kmod1,newdata=sexcomp)
+s1
+
+ls(s1)
+
+summary(s1)
+
+head(s1$cumhaz)
+
+ls(s1)
+
+
+S = s1$surv[1:10,1] # Col 1 is males
+H = s1$cumhaz[1:10,1]
+Q = exp(-H) # Question: Is this the survival function?
+cbind(H,S,Q)
+
+plot(s1)
+# Try to locate the medians
+xx = c(0,400); yy = c(.5,0.5)
+lines(xx,yy,col="red")
+# Median for M = 26, F = 141 ?
+
+
+# Median for M = 26, F = 141 ?
+xm = c(26,26); ym = c(0,1); lines(xm,ym,col="red")
+xf = c(141,141); yf = c(0,1); lines(xf,yf,col="blue")
+
+# How about a nicer plot?
+plot(s1,lty = c(1,2),xlab="Days", ylab="Probability")
+title('Estimated "Survival" Probabilities for the Catheter')
+
+xm = c(350,450); ym = c(0.9,0.9); lines(xm,ym,lty=1)
+text(500,0.9,"Males  ")
+
+xf = c(350,450); yf = c(0.8,0.8); lines(xf,yf,lty=2)
+text(500,0.8,"Females")
+# Compare disease types, just for women
+table(Kidney$disease)
+
+Other = data.frame(age=0, sex=1, disease="Other", frail=0)
+GN = data.frame(age=0, sex=1, disease="GN", frail=0)
+AN = data.frame(age=0, sex=1, disease="AN", frail=0)
+PKD = data.frame(age=0, sex=1, disease="PKD", frail=0)
+discomp = rbind(Other, GN, AN, PKD)
+rownames(discomp) = c("Other", "GN", "AN", "PKD")
+s2 = survfit(kmod1,newdata=discomp); s2
+
+summary(kmod1)
+
+# Catheters for patients with PKD stay in longest.
+# How about some pairwise comparisons?
+
+s2
+
+plot(s2,lty = 1:4,xlab="Days", ylab="Probability")
+title('Estimated "Survival" Probabilities by Disease Type')
